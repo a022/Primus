e819c3bbc7ab6d2962a1c1cc82cb2249d89baeae
Andrew Cafourek <andrew@a022digital.com>
2018-01-03 11:25:02 -0500

    Remove all styles

--- footer.php
+++ footer.php
@@ -14,18 +14,8 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'primus' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'primus' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'primus' ), 'primus', '<a href="https://automattic.com/">Automattic</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- functions.php
+++ functions.php
@@ -24,9 +24,6 @@ if ( ! function_exists( 'primus_setup' ) ) :
 		 */
 		load_theme_textdomain( 'primus', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
-
 		/*
 		 * Let WordPress manage the document title.
 		 * By adding theme support, we declare that this theme does not use a
@@ -44,7 +41,7 @@ if ( ! function_exists( 'primus_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'primus' ),
+			'primary-nav' => esc_html__( 'Primary Navigation', 'primus' ),
 		) );
 
 		/*
@@ -53,17 +50,10 @@ if ( ! function_exists( 'primus_setup' ) ) :
 		 */
 		add_theme_support( 'html5', array(
 			'search-form',
-			'comment-form',
 			'comment-list',
 			'gallery',
 			'caption',
 		) );
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'primus_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
 
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
@@ -119,20 +109,9 @@ add_action( 'widgets_init', 'primus_widgets_init' );
 function primus_scripts() {
 	wp_enqueue_style( 'primus-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'primus-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'primus-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'primus_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -148,13 +127,6 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 
 /**
  * Load WooCommerce compatibility file.
